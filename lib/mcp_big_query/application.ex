88c854a7e1f158893fb5d30014fe70945fc59100
javierg <tijuanate@gmail.com>
2025-12-16 13:50:02 -0800

    allow config for http and mcp servers

--- lib/mcp_big_query/application.ex
+++ lib/mcp_big_query/application.ex
@@ -4,8 +4,8 @@ defmodule McpBigQuery.Application do
   def start(_type, _args) do
     children = [
       Hermes.Server.Registry,
-      {McpBigQuery.Server, transport: {:streamable_http, base_url: "http://localhost:4022"}},
-      {Bandit, plug: McpBigQuery.Router, port: System.get_env("PORT") || 4000}
+      {McpBigQuery.Server, transport: {:streamable_http, base_url: server_url()}},
+      {Bandit, plug: McpBigQuery.Router, port: http_port()}
     ] ++ goth()
 
     Supervisor.start_link(children, strategy: :one_for_one)
@@ -23,5 +23,14 @@ defmodule McpBigQuery.Application do
         source: {:service_account, credentials, [scopes: ["https://www.googleapis.com/auth/cloud-platform"]]}
       }
     ]
+  end
+
+  defp http_port do
+    Application.get_env(:mcp_big_query, :http_port) || 4000
+  end
+
+  defp server_url do
+    Application.get_env(:mcp_big_query, :server_url) ||
+      "http://localhost:4001"
   end
 end
