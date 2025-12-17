a0368f4cee8930bdcdb25797c336f4c081026a4e
javierg <tijuanate@gmail.com>
2025-12-17 15:08:38 -0800

    add test options

--- lib/mcp_big_query/application.ex
+++ lib/mcp_big_query/application.ex
@@ -8,9 +8,16 @@ defmodule McpBigQuery.Application do
       [
         Hermes.Server.Registry,
         {McpBigQuery.Server, transport: {:streamable_http, base_url: ServerConfig.server_url()}},
-        {Bandit, plug: McpBigQuery.Router, port: ServerConfig.http_port()}
-      ] ++ GothConfig.child_spec()
+      ] ++ GothConfig.child_spec() ++ http_server()
 
     Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
+  defp http_server do
+    if Application.get_env(:mcp_big_query, :start, true) do
+      [{Bandit, plug: McpBigQuery.Router, port: ServerConfig.http_port()}]
+    else
+      []
+    end
   end
 end
